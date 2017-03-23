4a9f8917fc5ef0d129c7e9e7a9d6c91570f63a3a
rotem.bu <rotem.bu@gmail.com>
2017-03-23 12:18:12 -0700

    First check in

---
+++ ConsoleApplication1/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSource
+{
+    class User
+    {
+        #region Properties 
+
+        /// <summary>
+        /// Job/Intership/Volunteer
+        /// </summary>
+        public string Typejobe { get; set; }
+        
+        /// <summary>
+        /// location of job
+        /// </summary>
+        public string Location { get; set; }
+        
+        /// <summary>
+        /// Area of focous of the job
+        /// </summary>
+        public string Areasoffocous { get; set; }
+        
+        /// <summary>
+        /// Employment type of the job like Full time/Part time
+        /// </summary>
+        public string Employmenttype { get; set; }
+
+        #endregion
+    }
+}
