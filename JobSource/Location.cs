5b7f048bc6353d74629314ffdd0d877f88947cbb
rotem.bu <rotem.bu@gmail.com>
2017-03-25 22:10:35 -0700

    renaming root folder adding gitignore file

---
+++ JobSource/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSource
+{
+    class Location
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string State { get; set; }
+    }
+}
